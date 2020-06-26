45f9d7be7026976a071ae1e541a2c93b17009f42
Guarany <33207513+carlosggomes@users.noreply.github.com>
2020-06-26 11:22:44 -0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,8 @@
   
 # virtual ip address of lbaas
 output "vip" {
-  value = "http://${ibm_lbaas.lbaas.vip}"
+  value = http://ibm_lbaas.lbaas.vip
+  
+  # Version 0.11.x
+  #  value = "http://${ibm_lbaas.lbaas.vip}"
 }
