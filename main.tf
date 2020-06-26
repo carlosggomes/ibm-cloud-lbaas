018897f16f66fe16af29db2646538738047ecaa9
Guarany <33207513+carlosggomes@users.noreply.github.com>
2020-06-26 18:42:56 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "ibm_lbaas" "lbaas" {
-  name        = "lbaas-cguarany4"
-  description = "created-by-schematics"
-  type        = "PRIVATE"
-  subnets     = [2088037]
+  name        = "test"
+  description = "updated desc-used for terraform uat"
+  subnets     = [1492041]
+ # datacenter  = "dal13"
 
   protocols {
     frontend_protocol     = "HTTP"
@@ -10,15 +10,13 @@ resource "ibm_lbaas" "lbaas" {
     backend_protocol      = "HTTP"
     backend_port          = 80
     load_balancing_method = "round_robin"
+    
   }
 }
 
 resource "ibm_lbaas_server_instance_attachment" "lbaas_member" {
   count              = 1
-  private_ip_address = "10.95.118.234"
+  private_ip_address = "10.95.58.114"
   weight             = 40
-  lbaas_id           = ibm_lbaas.lbaas.id
-  
-  # Versao 0.11.x
-  #  lbaas_id           = "${ibm_lbaas.lbaas.id}"
+  lbaas_id           = "${ibm_lbaas.lbaas.id}"
 }
