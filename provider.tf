86c9d07c9411bf84b3d740b2cd2ade67f721a63e
Guarany <33207513+carlosggomes@users.noreply.github.com>
2020-06-26 11:27:24 -0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,11 +1,9 @@
 variable "iaas_classic_username" {}
 variable "iaas_classic_api_key" {}
+variable "ibmcloud_api_key" {}
 
 provider "ibm" {
   iaas_classic_username = var.iaas_classic_username
   iaas_classic_api_key  = var.iaas_classic_api_key
- 
-  # Version 0.11.x
-  # iaas_classic_username = "${var.iaas_classic_username}"
-  # iaas_classic_api_key  = "${var.iaas_classic_api_key}"
+  ibmcloud_api_key = var.ibmcloud_api_key
 }
