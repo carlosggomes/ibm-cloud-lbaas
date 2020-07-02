020621a498279dfd3280575ab63f714a2117ab92
Guarany <33207513+carlosggomes@users.noreply.github.com>
2020-07-02 15:37:30 -0300

    Multiple IP
    Multiple IP

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "ibm_lbaas" "lbaas" {
-  name        = "test"
-  description = "Load Balance created via Schematics - Terraform"
+  name        = "cguarany-test2"
+  description = "Load Balance created via Schematics - Terraform - multiple IP"
   subnets     = [1492041]
  # datacenter  = "dal13"
 
@@ -15,8 +15,8 @@ resource "ibm_lbaas" "lbaas" {
 }
 
 resource "ibm_lbaas_server_instance_attachment" "lbaas_member" {
-  count              = 1
-  private_ip_address = "10.95.58.114"
+ # count              = 1
+  private_ip_address = ["10.95.58.114" , "10.95.58.115"]
   weight             = 40
   lbaas_id           = "${ibm_lbaas.lbaas.id}"
 }
